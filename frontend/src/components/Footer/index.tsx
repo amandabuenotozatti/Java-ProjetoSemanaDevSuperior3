8f28835f04eb4a7c2a3d0b879176f51cc4e3b38d
Amanda Tozatti <amandabuenotozatti@gmail.com>
2021-09-10 22:48:19 -0300

    DonutChart integration

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">App desenvolvido por <a href="https://github.com/amandabuenotozatti" target="_blank" rel="noreferrer">Amanda Tozatti</a></p>
-        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+        <p className="text-light">App desenvolvido por  <a href="https://github.com/amandabuenotozatti" target="_blank" rel="noreferrer">Amanda Tozatti</a></p>
+        <p className="text-light"><small><strong> Semana Spring React</strong><br />
           Evento promovido pela escola DevSuperior: <a href="https://instagram.com/amandabuenotozatti" target="_blank" rel="noreferrer">@amandabuenotozatti</a></small></p>
       </div>
     </footer>
